07c4651cb7aef3d6b8e70f1b3d50233834b3fddf
Raoni Alves Rocha <raoniguimao@gmail.com>
2020-10-20 00:18:13 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = {x:8 * box, y: 8 * box};
+snake[0] = {
+    x:8 * box,
+    y: 8 * box
+}
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -18,7 +25,19 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down")  direction = "up";
+    if(event.keyCode == 39 && direction != "left")  direction = "right";
+    if(event.keyCode == 40 && direction != "up")    direction = "down";
+}
 
 function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
@@ -28,6 +47,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -44,13 +64,6 @@ function iniciarJogo(){
     let newHead = { x: snakeX, y: snakeY };
 
     snake.unshift( newHead );
-}
-
-function update(event){
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down")  direction = "up";
-    if(event.keyCode == 39 && direction != "left")  direction = "right";
-    if(event.keyCode == 40 && direction != "up")    direction = "down";
 }
 
 let jogo = setInterval(iniciarJogo, 100);
